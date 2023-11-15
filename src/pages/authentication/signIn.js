7c2543ae866b8cdea29f92c498c81c8eae1f05f8
sipoufo <sipoufoknj@gmail.com>
2023-11-15 14:45:19 +0100

    feat: start and end admin integration

--- src/pages/authentication/signIn.js
+++ src/pages/authentication/signIn.js
@@ -4,32 +4,41 @@ import { Link } from "react-router-dom";
 import ButtonWidget from "../../widgets/buttonWidget";
 import { SignInService } from "../../services/authenticationService";
 import LoadingComponent from "../../components/loadingComponent";
+import AlertMessageComponent from "../../components/alertMessageComponent";
 
 const SignIn = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [activeMessageBox, setActiveMessageBox] = useState(false);
+    const [data, setData] = useState(false);
 
     const SignIn = async (e) => {
         e.preventDefault();
         setLoading(true);
         const data = {
             username: email,
-            password
-        }
+            password,
+        };
         const response = await SignInService(data);
-        console.log(response);
-        // setTicketTypes(data.data);
+        if (response.isError) {
+            setActiveMessageBox(true);
+            setData(response);
+        }
         setLoading(false);
     };
 
     if (loading) {
-        <LoadingComponent />
+        <LoadingComponent />;
     }
     return (
         <div className="w-10/12 sm:w-8/12 md:w-6/12 lg:w-3/12 flex flex-col items-center justify-center gap-10">
             {/* <h1 className="font-extrabold text-3xl text-primary">MyTicket</h1> */}
-            <img src={process.env.PUBLIC_URL + "/assets/logos/logo.png"} className="w-full object-contain" alt="logo" />
+            <img
+                src={process.env.PUBLIC_URL + "/assets/logos/logo.png"}
+                className="w-full object-contain"
+                alt="logo"
+            />
             <form className="flex flex-col gap-4 w-full" onSubmit={SignIn}>
                 {/* Email */}
                 <InputWidget
@@ -50,11 +59,23 @@ const SignIn = () => {
                     placeholder={"password"}
                 />
                 <div className="flex justify-end">
-                    <Link to={"/forgetPassword"} className="text-primary underline underline-offset-2 hover:no-underline">Forgot Password ?</Link>
+                    <Link
+                        to={"/forgetPassword"}
+                        className="text-primary underline underline-offset-2 hover:no-underline"
+                    >
+                        Forgot Password ?
+                    </Link>
                 </div>
                 {/* Submit Button */}
                 <ButtonWidget name={"Login"} />
             </form>
+            <AlertMessageComponent
+                isActive={activeMessageBox}
+                title={"Error"}
+                message={data["message"]}
+                setIsActive={setActiveMessageBox}
+                isError={true}
+            />
         </div>
     );
 };
