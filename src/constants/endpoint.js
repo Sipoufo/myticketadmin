9bea08630113951f2c8a91e632e9544a00b81d6e
camcoder337 <camcoder337@gmail.com>
2024-01-09 15:00:30 +0100

    Add Organizer Request endpoint

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -4,3 +4,6 @@ const URL = process.env.REACT_APP_API_URL;
 export const SignInEndpoint = () => URL + "/api/auth/signin";
 export const ForgetPasswordEndpoint = () => URL + "/api/auth/forgetPassword";
 export const ResetPasswordEndpoint = () => URL + "/api/auth/resetPassword";
+
+// Organizer Request
+export const OrganizerRequest_EndPoint = (pageNumber, dataSize) => URL + "/api/requestOrganizer/" + pageNumber + "/" + dataSize;
